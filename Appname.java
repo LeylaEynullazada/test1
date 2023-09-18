a408b7585fd7676196ade95de59ead7addae4567
leyla_eynullazada_18033 <leynullazada18033@ada.edu.az>
2023-09-18 20:33:26 +0400

    hello

---
+++ Appname.java
@@ -0,0 +1,9 @@
+class Appname {
+    int a=5;
+    public static void main(String args[]) {
+        for (int i=0; i<5; i++) {
+            int x=5;
+            System.out.println("x="+ x++);
+        }
+    }
+}
---
+++ ParamApp.java
@@ -0,0 +1,6 @@
+class ParamApp {
+    public static void main(String args[]) {
+        System.out.println("The first parameter is: " + args[0]);
+        System.out.println("The second parameter is: " + args[1]); 
+    }
+}
